dcd15fef01a51afdd7fee986539d264946a6f1b9
denigiu <38814108+denigiu@users.noreply.github.com>
2018-06-15 18:17:48 +0200

    adding starting project

---
+++ script.js
@@ -0,0 +1,15 @@
+const array = [1, 2, 10, 16];
+
+const double = []
+const newArray = array.forEach((num) => {
+    
+    double.push(num*2);
+    
+})
+
+console.log(double);
+
+const mapArray = array.map((num) {
+                           return num*2
+                        });
+console.log(mapArray);
